2515ad90c01f26304b27067e54bc4c76d814ed76
wjaszczak <jaszczakw@gmail.com>
2014-09-21 18:15:19 +0200

    Poprawione wyliczanie i agregowanie.

--- src/StreamDataGenerator.java
+++ src/StreamDataGenerator.java
@@ -11,13 +11,13 @@ import org.apache.commons.math.stat.descriptive.summary.Sum;
 
 
 public class StreamDataGenerator {
-	public Long deviationTimeToNextClient = 40l;
+	public Long deviationTimeToNextClient = 60l;
 	public Long meanTimeToNextClient = 60l;
 	public Long deviationAmountToTank = 15l;
 	public Long meanAmountToTank = 35l;
 	public Double msTimeFor1l = 1800d; 	// 1l/1.8s
 	public Integer interval = 5000; // 5s
-	public Long aggregationInterval = 900000l; // 15min
+	public int aggregationInterval = 900000; // 15min
 	public Long minuteToMs = 60000l;
 	
 	
@@ -25,7 +25,7 @@ public class StreamDataGenerator {
 		
 	}
 
-	public Double getNexValueToTankInL(Random randomGenerator) {
+	public Double getRandValueToTankInL(Random randomGenerator) {
 		return randomGenerator.nextGaussian() * deviationAmountToTank
 				+ meanAmountToTank;
 	}
@@ -37,51 +37,54 @@ public class StreamDataGenerator {
 	
 	public Map<Calendar,Double> Generate(Calendar dateFrom, Calendar dateTo){
 
-		Calendar from = (Calendar) dateFrom.clone();
-		Calendar till = (Calendar) dateTo.clone();
+		Calendar startingDate = (Calendar) dateFrom.clone();
+		Calendar endDate = (Calendar) dateTo.clone();
 
-		Long ticksAmount = (till.getTimeInMillis() - from.getTimeInMillis())
+		Long ticksAmount = (endDate.getTimeInMillis() - startingDate.getTimeInMillis())
 				/ interval;
 		
 		Random randomGenerator = new Random();
 		Double nextTank = nextTankInMinutes(randomGenerator) * minuteToMs;
 		
 		Map<Calendar,Double> dict =  new HashMap<Calendar, Double>();
+		ArrayList<Double> values = new ArrayList<Double>();
 
 		Double amountOfFuelInTick= interval/msTimeFor1l;
 		Double valueToTank = 0d;
 		
-		for (Integer i = 0; i < ticksAmount; i++, nextTank -= interval, from.add(Calendar.MILLISECOND, interval)) {
+		for (Integer i = 0; i < ticksAmount; i++, nextTank -= interval, startingDate.add(Calendar.MILLISECOND, interval)) {
 			if (nextTank <= 0 && valueToTank<=0) {
-				valueToTank = Math.max(0,getNexValueToTankInL(randomGenerator));
+				valueToTank = Math.max(0,getRandValueToTankInL(randomGenerator));
 				nextTank = nextTankInMinutes(randomGenerator) * minuteToMs;
 			}
 			
 			if(valueToTank>0){
 				System.out.print(Math.min(amountOfFuelInTick, valueToTank));
-				System.out.println(" \t" + from.getTime());
-				dict.put(from, Math.min(amountOfFuelInTick, valueToTank));
-				valueToTank-= interval/msTimeFor1l;
+				System.out.println(" \t" + startingDate.getTime());
+				double tankedValue = Math.min(amountOfFuelInTick, valueToTank);
+				dict.put(startingDate, tankedValue);
+				values.add(tankedValue);
+				valueToTank-= tankedValue;
 			} else{
-				System.out.println("0 \t" + from.getTime());
-				dict.put(from, 0d);
+				System.out.println("0 \t" + startingDate.getTime());
+				dict.put(startingDate, 0d);
+				values.add(0d);
 			}
 		}
 		
-		
-		double[] primitiveArray = ArrayUtils.toPrimitive(dict.values().toArray(new Double[dict.values().size()]));
+		//.toArray()
+//		double[] primitiveArray = ArrayUtils.toPrimitive(dict.values().toArray(new Double[dict.values().size()]));
+		double[] primitiveArray = ArrayUtils.toPrimitive(values.toArray(new Double[values.size()]));
 
-		int step = (int) (aggregationInterval/interval);
-//		for(int i = 0; i<ticksAmount; i+=step){
-//			System.out.println(sum.evaluate(primitiveArray,i,step) +" \t" + from.getTime());
-//		}
 		
-		aggregateData(step,primitiveArray);
+		int step = (int) (aggregationInterval/interval);
+		
+		aggregateData(step,primitiveArray,(Calendar)dateFrom.clone());
 		
 		return dict;
 	}
 	
-	public double[] aggregateData(int step,double[] array){
+	public double[] aggregateData(int step,double[] array, Calendar startDate){
 		Sum sum = new Sum();
 		
 		ArrayList<Double> aggregatedData = new ArrayList<Double>();
@@ -89,7 +92,8 @@ public class StreamDataGenerator {
 		for(int i = 0; i<array.length; i+=step){
 			double result = sum.evaluate(array,i,step);
 			aggregatedData.add(result);
-			System.out.println(result +" \t" + i);
+			System.out.println(result +" \t" + startDate.getTime());
+			startDate.add(Calendar.MILLISECOND, aggregationInterval);
 		}
 		
 		return null;
